c8abd8697b1e661718c17a378d34608bc0212f07
JamysonSantos <144974409+JamysonSantos@users.noreply.github.com>
2024-05-04 21:29:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	</div>
         <img src="PlanestitP.png" class="absolute bottom-0 right-0 w-12 h-auto border rounded">
     </div>
-  ;
+  `;
     // Cria um novo elemento div para o card de ação
     var newCard = document.createElement('div');
     newCard.classList.add('action-card'); // Adiciona a classe 'action-card' ao novo card
